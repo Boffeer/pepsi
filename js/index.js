79e54d0f09cde92c11f4005194802f11271d9068
Boffeer <41575886+Boffeer@users.noreply.github.com>
2022-07-21 19:06:50 +0300

    add player

--- js/index.js
+++ js/index.js
@@ -62,3 +62,70 @@ setTimeout(() => {
     }
   });
 }, 150);
+
+/**
+ * Youtube
+ */
+// 2. This code loads the IFrame Player API code asynchronously.
+// var tag = document.createElement("script");
+
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName("script")[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// // 3. This function creates an <iframe> (and YouTube player)
+// //    after the API code downloads.
+// var player;
+// function onYouTubeIframeAPIReady() {
+//   player = new YT.Player("player", {
+//     width: "100%",
+//     videoId: "XpRbFYtR0_A",
+//     playerVars: { autoplay: 1, playsinline: 1 },
+//     events: {
+//       onReady: onPlayerReady,
+//     },
+//   });
+// }
+
+// // 4. The API will call this function when the video player is ready.
+// function onPlayerReady(event) {
+//   event.target.mute();
+//   event.target.playVideo();
+// }
+
+// ---
+
+// Load the IFrame Player API code asynchronously.
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// Replace the 'ytplayer' element with an <iframe> and
+// YouTube player after the API code downloads.
+var player;
+function onYouTubePlayerAPIReady() {
+  player = new YT.Player("ytplayer", {
+    videoId: "XpRbFYtR0_A",
+    events: {
+      onStateChange: cahngePlayerState,
+    },
+    playerVars: {
+      autoplay: 1,
+      showinfo: 0,
+      controls: 0,
+      mute: 1,
+      loop: 1,
+      rel: 0,
+      version: 3,
+    },
+  });
+}
+
+function cahngePlayerState(data) {
+  console.log(data);
+  if (data.data == 1) {
+    document.querySelector(".hero-bg__video-wrap").style.display = "block";
+  } else if (data.data == 0) {
+    document.querySelector(".hero-bg__video-wrap").remove();
+  }
+}
